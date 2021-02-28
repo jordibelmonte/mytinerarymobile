48e71d2179c06ef186f9e016f9a3b0b08aab56fc
Jordi Belmonte <jorte.03@gmail.com>
2021-02-28 02:41:29 -0300

    itinerary almost ready

--- screens/Itineraries.js
+++ screens/Itineraries.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, ImageBackground, Button, TextBase, Pressable, TouchableHighlight, SafeAreaView, ScrollView, TextInput, Flatlist } from 'react-native';
 import {useEffect, useState} from 'react'
+import Itinerary from './Itinerary'
 
 const Itineraries = (props) => {
     const cityId = props.route.params.idCity
     const [itineraries, setItineraries] = useState([])
-    const [visible, setVisible] = useState(!visible)
+    
     useEffect(()=>{
         fetch('https://mytinenarymobile.herokuapp.com/api/itineraries/'+cityId)
         .then(response => response.json())
@@ -17,36 +18,7 @@ const Itineraries = (props) => {
     <>
     <ScrollView>
         { itineraries.map(itinerary => (
-            <View style={styles.contenedorPadre}>
-            <View style={styles.contenedorItinerario}>
-                <Text style={styles.tituloItinerario}>{itinerary.title}</Text>
-                <Text style={styles.autor}>{itinerary.userName}</Text>
-                <View style={styles.contenedorDatos}>
-                    <View ><Text style={styles.texto}>4 Likes</Text></  View>
-                    <View ><Text style={styles.texto}>Duration: {itinerary.duration}hs</    Text></View >
-                    <View ><Text style={styles.texto}>{Array(itinerary.price).fill('$')}</Text></  View>
-                </View>
-                <View style={styles.hashtagContenedor}>
-                    {itinerary.hashtags.map(hashtag => (<Text style={styles.hashtag}>{hashtag}</Text>))}
-                </View>
-                <TouchableHighlight onPress={() => {setVisible(!visible)}}>
-                    {!visible ?
-                    <View style={styles.contenedorVisible}>
-                        <Text style={styles.visible}>View More</Text>
-                    </View>  : <View>
-                        <View style={styles.actividades}> 
-                            <View style={styles.actividadContenedor}>
-                                <Image style={styles.imgActividad}/>
-                                <Text style={styles.actividadTitulo}>Titulo     Actividad</Text>
-                            </View>
-                        </View>
-                        <View style={styles.contenedorVisible}>
-                            <Text style={styles.visible}>View Less</Text>
-                        </View>
-                    </View>}
-                </TouchableHighlight>
-            </View>
-        </View>
+          <Itinerary key={itinerary.title} props={itinerary}/>
         
     ))}
     
@@ -68,82 +40,6 @@ const Itineraries = (props) => {
 
     </>)
 }
-const styles ={
-    contenedorPadre:{
-        flex:1,
-        alignItems:'center'
-    },
-    contenedorItinerario:{
-        flex:1,
-        backgroundColor:'#050A1A',
-        marginTop: '3%',
-        marginBottom: '3%',
-        alignItems:'center',
-        width: '90%',
-        borderRadius: 20,
-        paddingTop: '3%',
-        paddingBottom: '3%',
-    },
-    tituloItinerario:{
-        fontSize: 30,
-        color: 'white',
-    },
-    texto:{
-        color:'white',
-        fontSize: 20,
-    },
-    contenedorDatos:{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        width:'100%'
-    },
-    hashtagContenedor:{
-        alignItems:'center'
-    },
-    hashtag:{
-        color:'white',
-        fontSize: 18,
-        paddingVertical: '5%'
-    },
-    visible:{
-        color:'white',
-        width:150,
-        textAlign:'center',
-        borderRadius:20,
-        backgroundColor: '#0072FF',
-        paddingVertical: 10,
-        paddingHorizontal: 30,
 
-    },
-    actividades:{
-        alignItems:'center',
-    },
-    actividadContenedor:{
-        display:'flex',
-        flex:1,
-        alignItems:'center',
-        width: '90%',
-        backgroundColor:'red',
-        margin: 20,
-    },
-    imgActividad:{
-        flex:1,
-        height: 200,
-        backgroundColor: 'red'
-    },
-    actividadTitulo:{
-        color:'white',
-        textAlign:'center',
-        fontSize: 16,
-    },
-    contenedorVisible:{
-        flex:1
-    },
-    autor:{
-        color:'white',
-        paddingVertical: 5,
-    }
 
-}
 export default Itineraries
--- screens/Itinerary.js
+++ screens/Itinerary.js
@@ -2,10 +2,122 @@ import React from 'react';
 import { StyleSheet, Text, View, Image, ImageBackground, Button, TextBase, Pressable, TouchableHighlight, SafeAreaView, ScrollView, TextInput, Flatlist } from 'react-native';
 import {useEffect, useState} from 'react'
 
-const Itinerary = () =>{
+
+const Itinerary = ({props}) =>{
+    const [visible, setVisible] = useState(false)
+    console.log(props)
  return (
      <>
+       <View style={styles.contenedorPadre}>
+            <View style={styles.contenedorItinerario}>
+                <Text style={styles.tituloItinerario}>{props.title}</Text>
+                <Text style={styles.autor}>{props.userName}</Text>
+                <View style={styles.contenedorDatos}>
+                    <View ><Text style={styles.texto}>4 Likes</Text></  View>
+                    <View ><Text style={styles.texto}>Duration: {props.duration}hs</    Text></View >
+                    <View ><Text style={styles.texto}>{Array(props.price).fill('$')}</Text></  View>
+                </View>
+                <View style={styles.hashtagContenedor}>
+                    {props.hashtags.map(hashtag => (<Text style={styles.hashtag}>{hashtag}</Text>))}
+                </View>
+                <TouchableHighlight onPress={() => {setVisible(!visible)}}>
+                    {!visible ?
+                    <View style={styles.contenedorVisible}>
+                        <Text style={styles.visible}>View More</Text>
+                    </View>  : <View>
+                        <View style={styles.actividades}> 
+                            {props.activities.map(activity => (
+                                <View style={styles.actividadContenedor}>
+                                    <Image style={styles.imgActividad} source={{uri: activity.activityPic}}/>
+                                    <Text style={styles.actividadTitulo}>{activity.activityTitle}</Text>
+                                </View>
+                            ))}
+                        </View>
+                        <View style={styles.contenedorVisible}>
+                            <Text style={styles.visible}>View Less</Text>
+                        </View>
+                    </View>}
+                </TouchableHighlight>
+            </View>
+        </View>
      </>
  )
+}
+const styles ={
+    contenedorPadre:{
+        flex:1,
+        alignItems:'center'
+    },
+    contenedorItinerario:{
+        flex:1,
+        backgroundColor:'#050A1A',
+        marginTop: '3%',
+        marginBottom: '3%',
+        alignItems:'center',
+        width: '90%',
+        borderRadius: 20,
+        paddingTop: '3%',
+        paddingBottom: '3%',
+    },
+    tituloItinerario:{
+        fontSize: 30,
+        color: 'white',
+    },
+    texto:{
+        color:'white',
+        fontSize: 20,
+    },
+    contenedorDatos:{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width:'100%'
+    },
+    hashtagContenedor:{
+        alignItems:'center'
+    },
+    hashtag:{
+        color:'white',
+        fontSize: 18,
+        paddingVertical: '5%'
+    },
+    visible:{
+        color:'white',
+        width:150,
+        textAlign:'center',
+        borderRadius:20,
+        backgroundColor: '#0072FF',
+        paddingVertical: 10,
+        paddingHorizontal: 30,
+
+    },
+    actividades:{
+        alignItems:'center',
+    },
+    actividadContenedor:{
+        display:'flex',
+        flex:1,
+        alignItems:'center',
+        width: '90%',
+        backgroundColor:'red',
+        margin: 20,
+    },
+    imgActividad:{
+        flex:1,
+        height: 200,
+        backgroundColor: 'red'
+    },
+    actividadTitulo:{
+        color:'white',
+        textAlign:'center',
+        fontSize: 16,
+    },
+    contenedorVisible:{
+        flex:1
+    },
+    autor:{
+        color:'white',
+        paddingVertical: 5,
+    }
 }
 export default Itinerary
